df3b82cb0354750580749cdf02cc4812d909ef1e
kimura miyuki <hcp.miyuki@gmail.com>
2020-01-19 21:23:17 +0900

    だいたいdone

--- tenki-app/RemainderViewController.swift
+++ tenki-app/RemainderViewController.swift
@@ -81,6 +81,8 @@ class RemainderViewController:UIViewController, UIPickerViewDelegate, UIPickerVi
     
     let weather_status_list = ["heyaboshi":"部屋干し推奨", "yoku":"よく乾く", "yaya":"やや乾く", "kawaku":"乾く", "taihen":"大変よく乾く"]
     
+    var alertController: UIAlertController!
+    
     
     @IBOutlet weak var remainder_status_label: UILabel!
     @IBOutlet weak var TimePickerView: UIPickerView!
@@ -98,22 +100,62 @@ class RemainderViewController:UIViewController, UIPickerViewDelegate, UIPickerVi
     
     @IBAction func set(_ sender: UIButton) {
         if (time_h != nil) && (time_m != nil) && (exponential_id != nil){
-            getWeatherStatus(row:exponential_id!)
+            let url = "http://127.0.0.1:5000/today-tomorrow-weather"
+                    var request = URLRequest(url: Foundation.URL(string: url)!)
+                    request.httpMethod = "GET"
+                    let session = URLSession.shared
+                    
+                    session.dataTask(with: request) {
+                        (data, response, error) in
+                        
+                        if error == nil {
+                            if let urlContent = data {
+                                DispatchQueue.main.async {
+                                    let weather_status:WeatherStatus = try! JSONDecoder().decode(WeatherStatus.self, from: urlContent)
+                                    if self.exponential_id! == 0{
+                                        self.status = weather_status.today
+                                    }else{
+                                        self.status = weather_status.tomorrow
+                                    }
+                                    
+
+                                    let content = UNMutableNotificationContent()
+                                    let center = UNUserNotificationCenter.current()
+                                                
+                                    //            登録済みの通知を削除
+                                    center.removeAllDeliveredNotifications()
+                                    if self.status != nil{
+                                        content.title = "\(self.exponential_choice_remainder[self.exponential_id!]):\(self.weather_status_list[self.status!]!)";
+                                        
+                                    }else{
+                                        content.title = "ぱや"
+                                    }
+                                                
+                                    content.body = "やほ";
+                                    content.sound = UNNotificationSound.default
+                                    let date = DateComponents(hour:self.time_h, minute:self.time_m)//(month:7, day:7, hour:12, minute:0)
+                                    let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: true)//1回だけならrepeatsをfalseに
+                                    let remainder_request = UNNotificationRequest.init(identifier: "TestNotification", content: content, trigger: trigger)
+                                                
+                                    center.add(remainder_request)
+                                    // Keyを指定して保存
+                                    self.userDefaults.set("\(self.time_h!)時\(self.time_m!)分", forKey: "time")
+                                    self.userDefaults.set(self.exponential_choice_remainder[self.exponential_id!], forKey: "exponential")
+                                    self.userDefaults.synchronize()
+                                    self.alert(title:"リマインダーを設定しました")
+                                    self.SetRemainderStatus()
+
+            //                        self.activityIndicatorView.stopAnimating()
+                                }
+                            }
+                            
+                        }else{
+                            print("err")
+                        }
+                            
+                    }.resume()
+                
             //以下で登録処理
-            let content = UNMutableNotificationContent()
-            if status != nil{
-                content.title = "\(exponential_choice_remainder[exponential_id!]):\(weather_status_list[status!]!)";
-            }else{
-                content.title = "ぱや"
-            }
-            
-            content.body = "やほ";
-            content.sound = UNNotificationSound.default
-            let date = DateComponents(hour:time_h, minute:time_m)//(month:7, day:7, hour:12, minute:0)
-            let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: true)//1回だけならrepeatsをfalseに
-            let request = UNNotificationRequest.init(identifier: "TestNotification", content: content, trigger: trigger)
-            let center = UNUserNotificationCenter.current()
-            center.add(request)
         }
         
     }
@@ -136,35 +178,18 @@ class RemainderViewController:UIViewController, UIPickerViewDelegate, UIPickerVi
         ExponentialPickerView.dataSource = self
     }
     
+    func alert(title:String) {
+        alertController = UIAlertController(title: title,
+                                            message: nil,
+                                   preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK",
+                                       style: .default,
+                                       handler: nil))
+        present(alertController, animated: true)
+    }
     
     func getWeatherStatus(row:Int){
+        print(row)
         
-        let url = "http://127.0.0.1:5000/today-tomorrow-weather"
-        var request = URLRequest(url: Foundation.URL(string: url)!)
-        request.httpMethod = "GET"
-        let session = URLSession.shared
-        
-        session.dataTask(with: request) {
-            (data, response, error) in
-            
-            if error == nil {
-                if let urlContent = data {
-                    DispatchQueue.main.async {
-                        let weather_status:WeatherStatus = try! JSONDecoder().decode(WeatherStatus.self, from: urlContent)
-                        if row == 0{
-                            self.status = weather_status.today
-                        }else{
-                            self.status = weather_status.tomorrow
-                        }
-
-//                        self.activityIndicatorView.stopAnimating()
-                    }
-                }
-                
-            }else{
-                print("err")
-            }
-                
-        }.resume()
     }
 }
